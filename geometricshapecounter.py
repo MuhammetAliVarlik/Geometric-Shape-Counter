66c13d6a2216bd38a13c54eced164594a82e2d9b
MuhammetAliVarlik <92933906+MuhammetAliVarlik@users.noreply.github.com>
2023-12-30 20:40:42 +0300

    v1.0.7
    Comment added, error check added

--- geometricshapecounter.py
+++ geometricshapecounter.py
@@ -3,11 +3,14 @@ Application to classify shapes
 """
 import cv2
 import json
+import math
 
 
 class GeometricShapeCounter:
     def __init__(self):
         """
+        Initialize function of class
+
         :param file(str): file name of input file
         """
         # temp variable to keep number of shapes
@@ -30,7 +33,8 @@ class GeometricShapeCounter:
         return defaultValues
 
     @staticmethod
-    def update_json_value(json_file_path, key, name, point, color, text, fontScale, thickness, fontColor,fillChoice,textChoice):
+    def update_json_value(json_file_path, key, name, point, color, text, fontScale, thickness, fontColor, fillChoice,
+                          textChoice):
         """
         Function to update settings of app (defaults.json)
 
@@ -43,6 +47,8 @@ class GeometricShapeCounter:
         :param fontScale: fontScale parameter in the json file (range [0-1]) (ex. 0.5)
         :param thickness: thickness parameter in the json file (ex. 1)
         :param fontColor: fontColor parameter in the json file in HEX format (ex. "#FFFFFF")
+        :param fillChoice: checkbox choice for fill
+        :param textChoice: checkbox choice for text
         :return: data: updated json file
 
         """
@@ -71,36 +77,56 @@ class GeometricShapeCounter:
         # write updated JSON data to file
         with open(json_file_path, 'w') as json_file:
             json.dump(data, json_file, indent=4)
-
+        # return updated version of json settings file
         return data
 
     @staticmethod
     def imRead(file):
         """
-        :param file:
-        :return:
+        Image import function
+
+        :param file:filepath of input image
+        :return:img in BGR format
         """
+        # read image with openCV
         img = cv2.imread(file)
         return img
 
     @staticmethod
     def imShow(img, windowName="Trial"):
         """
+        Image displaying function
 
-        :param img:
-        :param windowName:
-        :return:
+        :param img:img to display
+        :param windowName: window name (default value=Trial)
+        :return:None
         """
+        # display image
         cv2.imshow(windowName, img)
         cv2.waitKey(0)
 
     def __im2Gray(self, img):
+        """
+        BGR to Grayscale Function
+
+        :param img:the image to be processed
+        :return:grayscale image
+        """
+        #convert bgr image to grayscale
         self.__img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return self.__img_gray
 
     @staticmethod
     def __threshold(img, thresh, maxVal):
-        # threshold for getting better results
+        """
+        Apply thresholding to the input image.
+
+        :param img: Input image
+        :param thresh: Threshold value
+        :param maxVal: Maximum value for thresholding
+        :return: Tuple containing threshold value and the thresholded image
+        """
+        # Apply thresholding to improve results
         ret, threshImg = cv2.threshold(img, thresh, maxVal, cv2.THRESH_BINARY)
         return ret, threshImg
 
@@ -109,12 +135,10 @@ class GeometricShapeCounter:
         """
         Convert a hexadecimal color code to BGR format.
 
-        Parameters:
-        - hex_color (str): Hexadecimal color code (e.g., "#1E90FF").
-
-        Returns:
-        - tuple: BGR color representation as a tuple.
+        :param hex_color: Hexadecimal color code (e.g., "#1E90FF").
+        :return:BGR color representation as a tuple.
         """
+        #return bgr color
         return tuple(int(hex_color[i:i + 2], 16) for i in (5, 3, 1))
 
     @staticmethod
@@ -128,31 +152,85 @@ class GeometricShapeCounter:
         Returns:
         - str: Hexadecimal color code.
         """
+        #return hex color
         return "#{:02X}{:02X}{:02X}".format(*bgr_color[::-1])
 
     def __findContours(self, img, thresh, maxVal):
-        grayImg=self.__im2Gray(img)
+        """
+        Find contours in the input image after thresholding.
+
+        :param img: Input image
+        :param thresh: Threshold value
+        :param maxVal: Maximum value for thresholding
+        :return: Tuple containing contours found, the thresholded image, and the grayscale image
+        """
+        # Convert the input image to grayscale
+        grayImg = self.__im2Gray(img)
+
+        # Apply thresholding to the grayscale image
         _, img = self.__threshold(grayImg, thresh=thresh, maxVal=maxVal)
-        # findContours finds the shell of shapes and classifies them hierarchically
+
+        # Use findContours to identify and hierarchically classify the shapes in the image
         contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        return contours,img,grayImg
+
+        return contours, img, grayImg
 
     def __findHierarchy(self, img, thresh, maxVal):
+        """
+        Find hierarchy of contours in the input image after thresholding.
+
+        :param img: Input image
+        :param thresh: Threshold value
+        :param maxVal: Maximum value for thresholding
+        :return: Hierarchy of contours found in the image
+        """
+        # Apply thresholding to the input image and convert it to grayscale
         _, img = self.__threshold(self.__im2Gray(img), thresh=thresh, maxVal=maxVal)
-        # findContours finds the shell of shapes and classifies them hierarchically
+
+        # Use findContours to identify contours and their hierarchical relationships
         _, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
         return hierarchy
 
-    def printCount(self,withLog):
+    def printCount(self, withLog):
+        """
+        Print the count if 'withLog' flag is True.
+
+        :param withLog: Flag to determine if the count should be printed
+        :return: The count if 'withLog' is True, otherwise None
+        """
         if withLog:
             return self.__count
 
     def __countShapes(self, shape):
+        """
+        Increment the count for a specific shape.
+
+        :param shape: Shape identifier to increment the count for
+        """
         self.__count[shape] += 1
 
     @staticmethod
-    def __fillPolyWithText(img, text, approx, x, y, fontScale, thickness, fontColor, color,fillChoice,textChoice, withText=True,
+    def __fillPolyWithText(img, text, approx, x, y, fontScale, thickness, fontColor, color, fillChoice, textChoice,
+                           withText=True,
                            withColor=True):
+        """
+        Fill a polygon with optional text and color.
+
+        :param img: Image to draw on
+        :param text: Text to display on the image
+        :param approx: Contour approximation of the shape
+        :param x: x-coordinate for text placement
+        :param y: y-coordinate for text placement
+        :param fontScale: Font scale for the text
+        :param thickness: Thickness of the text
+        :param fontColor: Color of the text
+        :param color: Fill color for the polygon
+        :param fillChoice: Flag to determine if the polygon should be filled
+        :param textChoice: Flag to determine if text should be displayed
+        :param withText: Flag to include text (default: True)
+        :param withColor: Flag to include color (default: True)
+        """
         m = cv2.moments(approx)
         cx = int(m["m10"] / m["m00"])
         cy = int(m["m01"] / m["m00"])
@@ -164,18 +242,37 @@ class GeometricShapeCounter:
                 cv2.circle(img, (cx, cy), 1, (0, 0, 255), 3)
         if withText:
             if bool(textChoice):
-                cv2.putText(img, "AL:"+str(round(cv2.arcLength(approx, True),2)), (x, y+15), cv2.FONT_HERSHEY_SIMPLEX, fontScale, fontColor, thickness)
-                cv2.putText(img, "A:" + str(round(cv2.contourArea(approx),2)), (x, y + 30),
+                cv2.putText(img, "AL:" + str(round(cv2.arcLength(approx, True), 2)), (x, y + 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, fontScale, fontColor, thickness)
+                cv2.putText(img, "A:" + str(round(cv2.contourArea(approx), 2)), (x, y + 30),
                             cv2.FONT_HERSHEY_SIMPLEX, fontScale, fontColor, thickness)
                 cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, fontScale, fontColor, thickness)
 
     @staticmethod
-    def __drawCircle(img, text, x, y, fontScale, thickness, fontColor, cnt, color,fillChoice,textChoice,withText=True, withColor=True):
-        ((x1, y1), radius) = cv2.minEnclosingCircle(cnt)
+    def __drawCircle(img, text, x, y, fontScale, thickness, fontColor, cnt, color, fillChoice, textChoice,
+                     withText=True, withColor=True):
+        """
+        Draw a circle with optional text and color.
+
+        :param img: Image to draw on
+        :param text: Text to display on the image
+        :param x: x-coordinate for text placement
+        :param y: y-coordinate for text placement
+        :param fontScale: Font scale for the text
+        :param thickness: Thickness of the text
+        :param fontColor: Color of the text
+        :param cnt: Contour defining the circle
+        :param color: Fill color for the circle
+        :param fillChoice: Flag to determine if the circle should be filled
+        :param textChoice: Flag to determine if text should be displayed
+        :param withText: Flag to include text (default: True)
+        :param withColor: Flag to include color (default: True)
+        """
+        (_, radius) = cv2.minEnclosingCircle(cnt)
         M = cv2.moments(cnt)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         if radius > 5:
-            # draw the circle and centroid on the frame,
+            # Draw the circle and centroid on the frame,
             # then update the list of tracked points
             if withColor:
                 if bool(fillChoice):
@@ -183,41 +280,63 @@ class GeometricShapeCounter:
                     cv2.circle(img, (int(center[0]), int(center[1])), 3, (0, 0, 255), -1)
         if withText:
             if bool(textChoice):
+                cv2.putText(img, "AL:" + str(round(2 * math.pi * radius, 2)), (x, y + 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, fontScale, fontColor, thickness)
+                cv2.putText(img, "A:" + str(round(math.pi * math.pow(radius, 2), 2)), (x, y + 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, fontScale, fontColor, thickness)
                 cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, fontScale, fontColor, thickness)
 
     def drawContours(self, img, eps=0.031, thresh=170, maxVal=255, closed=True, withText=True, withColor=True,
                      withLog=True):
+        """
+        Draw contours on the input image with optional text and color.
 
-        contours,thresholdImage,grayImage = self.__findContours(img, thresh=thresh, maxVal=maxVal)
+        :param img: Input image to draw contours on
+        :param eps: Approximation accuracy for contours (default: 0.031)
+        :param thresh: Threshold value for contour identification (default: 170)
+        :param maxVal: Maximum value for thresholding (default: 255)
+        :param closed: Flag for closed contours (default: True)
+        :param withText: Flag to include text in the image (default: True)
+        :param withColor: Flag to include color in the image (default: True)
+        :param withLog: Flag to print the count (default: True)
+        :return: Tuple containing the modified image, count dictionary, count information, thresholded image, and grayscale image
+        """
+        contours, thresholdImage, grayImage = self.__findContours(img, thresh=thresh, maxVal=maxVal)
+
         for cnt in contours:
             epsilon = eps * cv2.arcLength(cnt, closed)
-            # use approxPolyDb to simplify poly lines (RDP based)
             approx = cv2.approxPolyDP(cnt, epsilon, closed)
             x = approx.ravel()[0]
             y = approx.ravel()[1]
-            # classify and paint
+
             if 6 < len(approx):
+                # Draw circle for shapes with more than 6 sides
                 default = self.defaults.get("{}".format(7))
                 self.__drawCircle(img, text=default["text"], x=x, y=y, fontScale=default["fontScale"],
                                   thickness=default["thickness"], fontColor=self.__hex_to_bgr(default["fontColor"]),
-                                  cnt=cnt, color=self.__hex_to_bgr(default["color"]),fillChoice=default["fillChoice"],textChoice=default["textChoice"],withText=withText,
-                                  withColor=withColor)
+                                  cnt=cnt, color=self.__hex_to_bgr(default["color"]), fillChoice=default["fillChoice"],
+                                  textChoice=default["textChoice"], withText=withText, withColor=withColor)
                 self.__countShapes(default["name"])
             elif len(approx) > 2:
+                # Fill polygon and display text for shapes with more than 2 sides
                 default = self.defaults.get("{}".format(len(approx)))
                 self.__fillPolyWithText(img=img, text=default["text"], approx=approx, x=x, y=y,
                                         fontScale=default["fontScale"], thickness=default["thickness"],
                                         fontColor=self.__hex_to_bgr(default["fontColor"]),
-                                        color=self.__hex_to_bgr(default["color"]),fillChoice=default["fillChoice"],textChoice=default["textChoice"],withText=withText,
-                                        withColor=withColor)
+                                        color=self.__hex_to_bgr(default["color"]), fillChoice=default["fillChoice"],
+                                        textChoice=default["textChoice"], withText=withText, withColor=withColor)
                 self.__countShapes(default["name"])
+
         if withLog:
             print(self.printCount(withLog=withLog))
-        return img, self.__count,self.printCount(withLog=withLog),thresholdImage,grayImage
+
+        return img, self.__count, self.printCount(withLog=withLog), thresholdImage, grayImage
 
 
-"""fileName = "shapes.png"
+"""
+fileName = "shapes.png"
 # fileName = ""
 gsc = GeometricShapeCounter(fileName)
 
-gsc.imShow(gsc.drawContours(gsc.original, withText=False, withColor=True))"""
+gsc.imShow(gsc.drawContours(gsc.original, withText=False, withColor=True))
+"""
--- main.py
+++ main.py
@@ -4,53 +4,132 @@ import base64
 import numpy as np
 import cv2
 import logging
-
+open('output_log.txt', 'w').close()
 logging.basicConfig(filename='web/output_log.txt', filemode='w', level=logging.DEBUG)
-
 eel.init("web")
 
 
 @eel.expose
-def initializeImage(data,eps,thresh,closed,withText,withColor,withLog):
+def initializeImage(data, eps, thresh, closed, withText, withColor, withLog):
+    """
+    Process input image data to identify geometric shapes and return processed images and count data.
+
+    :param data: Base64-encoded image data
+    :param eps: Approximation accuracy for contours
+    :param thresh: Threshold value for contour identification
+    :param closed: Flag for closed contours
+    :param withText: Flag to include text in the image
+    :param withColor: Flag to include color in the image
+    :param withLog: Flag to log count information
+    :return: Processed image, count of shapes, thresholded image, grayscale image as base64 strings
+    """
     try:
-        image_data = data.split(';base64,')[-1]  # Base64 verisini ayırma
-        binary_data = base64.b64decode(image_data)  # Base64 verisini çözme
+        # Decode base64 image data and convert to OpenCV format
+        image_data = data.split(';base64,')[-1]
+        binary_data = base64.b64decode(image_data)
         np_data = np.frombuffer(binary_data, np.uint8)
         inp = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+
+        # Initialize GeometricShapeCounter
         gsc = GeometricShapeCounter()
-        image,count,log,thresholdImage,grayImage=gsc.drawContours(inp, eps=float(eps),thresh=int(thresh),closed=closed,withText=withText,withColor=withColor,withLog=withLog)
-        print(thresholdImage)
+
+        # Process the input image to detect shapes and generate outputs
+        image, count, log, thresholdImage, grayImage = gsc.drawContours(
+            inp,
+            eps=float(eps),
+            thresh=int(thresh),
+            closed=closed,
+            withText=withText,
+            withColor=withColor,
+            withLog=withLog
+        )
+
+        # Encode processed images back into base64
         retval, buffer = cv2.imencode('.jpg', image)
         jpg_as_text = base64.b64encode(buffer.tobytes())
+
         retvalThresh, bufferThresh = cv2.imencode('.jpg', thresholdImage)
         jpg_as_text_thresh = base64.b64encode(bufferThresh.tobytes())
+
         retvalGray, buffer_Gray = cv2.imencode('.jpg', grayImage)
         jpg_as_text_gray = base64.b64encode(buffer_Gray.tobytes())
-        print(jpg_as_text_thresh)
-        logging.info(str(log),exc_info=True)
-        return 'data:image/jpeg;base64,' + jpg_as_text.decode('utf-8'), count,'data:image/jpeg;base64,' + jpg_as_text_thresh.decode('utf-8'),'data:image/jpeg;base64,' + jpg_as_text_gray.decode('utf-8')
+
+        # Log information and return processed images and count data as base64 strings
+        logging.info(str(log), exc_info=True)
+
+        return (
+            'data:image/jpeg;base64,' + jpg_as_text.decode('utf-8'),  # Processed image
+            count,  # Count of shapes
+            'data:image/jpeg;base64,' + jpg_as_text_thresh.decode('utf-8'),  # Thresholded image
+            'data:image/jpeg;base64,' + jpg_as_text_gray.decode('utf-8')  # Grayscale image
+        )
     except Exception as e:
+        # Error handling and logging for exceptions
         logging.error("Exception occurred", exc_info=True)
+        return "error"
+
+
 
 
 @eel.expose
-def getDefaults(key,property_name):
-    gsc=GeometricShapeCounter()
-    defaults=gsc.get_defaults()
-    # Check if the key exists in the data
-    if str(key) in defaults:
-        shape = defaults[str(key)]
-        for prop, value in shape.items():
-            if prop == property_name:
-                return value
-    return None
+def getDefaults(key, property_name):
+    """
+    Get default properties of a geometric shape.
+
+    :param key: Key representing the shape
+    :param property_name: Name of the property to retrieve
+    :return: Value of the specified property for the given shape key or None if not found
+    """
+    try:
+        gsc = GeometricShapeCounter()
+        defaults = gsc.get_defaults()
+
+        # Check if the key exists in the default data
+        if str(key) in defaults:
+            shape = defaults[str(key)]
+            for prop, value in shape.items():
+                # Find the specified property for the shape
+                if prop == property_name:
+                    return value
+        return None  # Return None if property or key is not found
+    except Exception as e:
+        # Error handling and logging for exceptions
+        logging.error("Exception occurred", exc_info=True)
+        return "error"  # Return "error" in case of exceptions
+
 
 @eel.expose
-def updateDefaults(key,property_name,point, color, text, fontScale, thickness, fontColor,fillChoice,textChoice):
-    gsc=GeometricShapeCounter()
-    filePath="defaults.json"
-    defaults=gsc.update_json_value(filePath, key, property_name,point, color, text, fontScale, thickness, fontColor,fillChoice,textChoice)
-    return "done"
+def updateDefaults(key, property_name, point, color, text, fontScale, thickness, fontColor, fillChoice, textChoice):
+    """
+    Update default properties of a geometric shape.
+
+    :param key: Key representing the shape
+    :param property_name: Name of the property to update
+    :param point: Point related to the property
+    :param color: Color related to the property
+    :param text: Text related to the property
+    :param fontScale: Font scale related to the property
+    :param thickness: Thickness related to the property
+    :param fontColor: Font color related to the property
+    :param fillChoice: Fill choice related to the property
+    :param textChoice: Text choice related to the property
+    :return: "done" if the update is successful, otherwise "error"
+    """
+    try:
+        gsc = GeometricShapeCounter()
+        filePath = "defaults.json"
+
+        # Update the JSON file with new property values for the specified shape key
+        defaults = gsc.update_json_value(
+            filePath, key, property_name, point, color, text, fontScale, thickness, fontColor, fillChoice, textChoice
+        )
+
+        return "done"  # Return "done" if the update is successful
+    except Exception as e:
+        # Error handling and logging for exceptions
+        logging.error("Exception occurred", exc_info=True)
+        return "error"  # Return "error" in case of exceptions
+
 
 # Start the index.html file
 eel.start("index.html")
